5fbcaec4607b1f97035f67d5461c86d01dc7991f
AdnanShah <adnanshah9911@gmail.com>
2018-06-10 08:22:36 -0700

    app init

---
+++ backend/config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "testsql",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql"
+  }
+};
