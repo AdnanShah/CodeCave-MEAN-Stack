a44c71489a0c6b3ee3b824984f0051a0b2213f24
AdnanShah <adnanshah9911@gmail.com>
2018-06-19 06:27:42 -0700

    dashboard updated

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
   selector: "app-dashboard",
@@ -6,9 +8,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  constructor(private _router: Router, private _userService: UserService) {
+    this._userService.posts().subscribe(
+      data => {
+        // console.log("data", data);
+        this.displayData(data);
+        // error => this._router.navigate(["/login"]);
+      },
+      error => console.error(error)
+    );
+  }
+  questions: Array<any> = [];
 
-  ngOnInit() {}
-
-
+  displayData(data) {
+    data.map((user, id) => {
+      // console.log(user);
+      // console.log(user.Questions);
+      // let obj = Object.assign({}, user.Questions);
+      return this.questions.push(...user.Questions);
+    });
+  }
+  ngOnInit() {
+    console.log("questions", this.questions);
+  }
 }
