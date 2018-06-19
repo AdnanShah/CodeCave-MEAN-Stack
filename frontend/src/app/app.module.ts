95948af66acea197de246306e416b31ed52c3f74
AdnanShah <adnanshah9911@gmail.com>
2018-06-18 17:28:55 -0700

    dashboard init

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserhomeComponent } from './userhome/userhome.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import {UserService} from './user.service';
-import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { UserhomeComponent } from "./userhome/userhome.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { UserService } from "./user.service";
+import { HttpClientModule } from "@angular/common/http";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -30,4 +32,4 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   providers: [UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
