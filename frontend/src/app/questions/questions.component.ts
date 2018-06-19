ac8a9ff31cc7fcf0a8b25ec1de749026c8ded9f8
AdnanShah <adnanshah9911@gmail.com>
2018-06-18 18:55:52 -0700

    questions set

--- frontend/src/app/questions/questions.component.ts
+++ frontend/src/app/questions/questions.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-questions',
-  templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.css']
+  selector: "app-questions",
+  templateUrl: "./questions.component.html",
+  styleUrls: ["./questions.component.css"]
 })
 export class QuestionsComponent implements OnInit {
+  askForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    username: new FormControl(null, Validators.required),
+    question: new FormControl(null, Validators.required)
+  });
+  constructor(private _router: Router, private _userService: UserService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  moveToLogin() {
+    this._router.navigate(["/login"]);
   }
 
+  askQuestion() {
+    if (!this.askForm.valid) {
+      console.log("Invalid Form");
+      return;
+    }
+
+    this._userService.register(JSON.stringify(this.askForm.value)).subscribe(
+      data => {
+        console.log(data);
+        this._router.navigate(["/login"]);
+      },
+      error => console.error(error)
+    );
+    console.log(this.askForm.value);
+  }
 }
