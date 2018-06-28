d034d4f9242aa763c8cc5b004d4014ac6058bac7
AdnanShah <adnanshah9911@gmail.com>
2018-06-28 05:25:48 -0700

    router updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserhomeComponent } from './userhome/userhome.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { UserhomeComponent } from "./userhome/userhome.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 import { PostsComponent } from "./posts/posts.component";
 import { QuestionsComponent } from "./questions/questions.component";
 
 const routes: Routes = [
-  {path:'', redirectTo:'login', pathMatch:'full'},
-  {path:'login', component:LoginComponent},
-  {path:'register', component:RegisterComponent},
-  {path:'user',component:UserhomeComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'posts',component:PostsComponent},
-  {path:'questions',component:QuestionsComponent}
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "user", component: UserhomeComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "posts", component: PostsComponent },
+  { path: "questions", component: QuestionsComponent },
+  { path: "**", redirectTo: "login" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
