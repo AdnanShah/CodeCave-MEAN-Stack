b5be8cc1d3de85e52f61617c371ce54a6861804e
AdnanShah <adnanshah9911@gmail.com>
2018-07-21 15:08:51 -0700

    dashboard end

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -18,56 +18,20 @@ export class DashboardComponent implements OnInit {
     );
   }
   questions: Array<any> = [];
-  hueGroups = [
-    {
-      name: "group 1",
-      lights: [
-        {
-          name: "light 1"
-        },
-        {
-          name: "light 2"
-        }
-      ]
-    },
-    {
-      name: "group 2",
-      lights: [
-        {
-          name: "light 3"
-        },
-        {
-          name: "light 4"
-        }
-      ]
-    }
-  ];
 
   displayData(data) {
     data.data.map((user, id) => {
       var a = user.answers;
-      // a = a.replace(/'/g, '"');
       a = JSON.parse(a);
-
       console.log("array", a);
-
-      // console.log("array", array);
-      // var array = user.answers.js;
-
-      this.questions.push(
-        // user
-        {
-          title: user.title,
-          question: user.question,
-          // answers: [1,2,3,4],
-          // answers: user.answers,
-          answers: a
-        }
-      );
+      this.questions.push({
+        title: user.title,
+        question: user.question,
+        answers: a
+      });
     });
   }
   ngOnInit() {
     console.log("questions", this.questions);
-    // console.log("answers", this.answers);
   }
 }
