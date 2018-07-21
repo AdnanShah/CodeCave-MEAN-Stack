c43f52f62c18841c8c40c9e8490073ef9a0c1f71
AdnanShah <adnanshah9911@gmail.com>
2018-07-21 14:47:06 -0700

    ans displayed

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -11,24 +11,63 @@ export class DashboardComponent implements OnInit {
   constructor(private _router: Router, private _userService: UserService) {
     this._userService.posts().subscribe(
       data => {
-        // console.log("data", data);
+        console.log(data);
         this.displayData(data);
-        // error => this._router.navigate(["/login"]);
       },
       error => console.error(error)
     );
   }
   questions: Array<any> = [];
+  hueGroups = [
+    {
+      name: "group 1",
+      lights: [
+        {
+          name: "light 1"
+        },
+        {
+          name: "light 2"
+        }
+      ]
+    },
+    {
+      name: "group 2",
+      lights: [
+        {
+          name: "light 3"
+        },
+        {
+          name: "light 4"
+        }
+      ]
+    }
+  ];
 
   displayData(data) {
     data.data.map((user, id) => {
-      // console.log(user);
-      // console.log(user.Questions);
-      // let obj = Object.assign({}, user.Questions);
-      return this.questions.push(user);
+      var a = user.answers;
+      // a = a.replace(/'/g, '"');
+      a = JSON.parse(a);
+
+      console.log("array", a);
+
+      // console.log("array", array);
+      // var array = user.answers.js;
+
+      this.questions.push(
+        // user
+        {
+          title: user.title,
+          question: user.question,
+          // answers: [1,2,3,4],
+          // answers: user.answers,
+          answers: a
+        }
+      );
     });
   }
   ngOnInit() {
     console.log("questions", this.questions);
+    // console.log("answers", this.answers);
   }
 }
